2e6ce6bb3e3169a58762d6c0faa4c73dc1dbd041
Ingryd <ingrydcduarte@gmail.com>
2025-06-28 11:51:45 -0300

    fix: passing app css to app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PrivateRoute from "./components/PrivateRoute";
 import { RouterProvider } from "react-router-dom";
 import { router } from "../app/routes/Routes";
+import "./App.css"
 
 const App = () => {
   return (
