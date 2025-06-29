06a8294af88884cd3e33d7db29750350793355af
Ingryd <ingrydcduarte@gmail.com>
2025-06-29 16:22:06 -0300

    feat: privateroute component (e utilização)

--- src/app/Components/PrivateRoute.jsx
+++ src/app/Components/PrivateRoute.jsx
@@ -1,10 +1,36 @@
 import { Navigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
 
-const PrivateRoute = ({ children }) => {
-    //TODO: Passar depois as lógicas de autenticação
-  const isAuthenticated = true;
+//Rotas que precisam estar autenticados dependendo do tipo
+//Uso: Você passa esse elemento dentro das rotas junto com o tipo permitido, mas antes das páginas filhas, tipo:
+/*
+{
+  path: "admin",
+  element: (
+    <PrivateRoute allowedTypes={["SECRETARIO"]}>
+      <AdminLayout />
+    </PrivateRoute>
+  ),
+  children: [
+    { index: true, element: <AdminDashboard /> },
+     //Outras páginas ou rotas aqui
+  ],
+}
+*/
+function PrivateRoute({ children, allowedTypes }) {
+  const { user } = useAuth();
 
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (!allowedTypes.includes(user.tipo)) {
+    // Usuário logado, mas tipo não permitido
+    return <Navigate to="/unauthorized" replace />;
+  }
+
+  // Usuário autorizado
+  return children;
+}
 
 export default PrivateRoute;
