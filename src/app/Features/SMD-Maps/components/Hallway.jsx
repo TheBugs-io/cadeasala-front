7943985ccd60c9e8c79d78934d1127670e51875a
Ingryd <ingrydcduarte@gmail.com>
2025-07-09 22:09:10 -0300

    feat: hallway (mapa)

---
+++ src/app/Features/SMD-Maps/components/Hallway.jsx
@@ -0,0 +1,11 @@
+import placeholderIcon from "../../../assets/placeholder/placeholder.png";
+const HallwayMap = () => {
+  return (
+    <div className="hallway-map">
+      <img src={placeholderIcon} alt="Placeholder for hallway map" />
+      <p>Comece por aqui.</p>
+    </div>
+  );
+};
+
+export default HallwayMap;
