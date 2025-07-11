4b6a91d03e6dbcc6b568f4282e003803eeda6316
Ingryd <ingrydcduarte@gmail.com>
2025-07-11 18:53:50 -0300

    up: updating tag status styles and changing to function

--- src/app/Features/DescSala/components/TagStatus.jsx
+++ src/app/Features/DescSala/components/TagStatus.jsx
@@ -1,11 +1,5 @@
 import '../styles/TagStatusStyle.css';
 
-const TagStatus = ({ status }) => {
-  return (
-    <div className="tag-status">
-      <h3>{status}</h3>
-    </div>
-  );
-};
-
-export default TagStatus;
+export default function TagStatus({ status }) {
+  return <div className={`tag-status status-${status}`}>{status}</div>;
+}
