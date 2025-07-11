6283dc7e8df75d7e37f00bc859153ba67529ad35
Ingryd <ingrydcduarte@gmail.com>
2025-07-10 23:42:35 -0300

    add: using label group with elevator icon

--- src/app/Features/SMD-Maps/components/AndarSelector.jsx
+++ src/app/Features/SMD-Maps/components/AndarSelector.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import '../styles/FloorSelector.css';
+import { PiElevatorFill } from "react-icons/pi";
 
 const FloorSelector = () => {
   const [andarSelecionado, setAndarSelecionado] = useState(1);
 
   return (
     <div className="floor-selector">
+      <div className="floor-header" aria-description='Seleção de Andar'>
+      <PiElevatorFill className="elevator-icon" size={32} />
       <h1 className="label">Andar</h1>
+      </div>
       <div className="options">
         {[1, 2].map((andar) => (
           <button
