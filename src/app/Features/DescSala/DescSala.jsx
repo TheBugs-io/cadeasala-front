1978831e6b83fc9d76adadc79e54224d79224be1
Ingryd <ingrydcduarte@gmail.com>
2025-07-11 18:53:35 -0300

    fix: styles in descsala

--- src/app/Features/DescSala/DescSala.jsx
+++ src/app/Features/DescSala/DescSala.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import { FaRegHeart } from "react-icons/fa";
 import TagStatus from "./components/TagStatus";
 import "./styles/RoomDetailsPage.css";
 
 export default function RoomDetails({ sala, status, dados, onClose }) {
-  const image = dados?.imagem || "https://conceito.de/wp-content/uploads/2019/04/sala-de-aula.jpg";
+  const image =
+    dados?.imagem ||
+    "https://conceito.de/wp-content/uploads/2019/04/sala-de-aula.jpg";
 
   return (
     <div className="room-overlay">
@@ -17,18 +19,17 @@ export default function RoomDetails({ sala, status, dados, onClose }) {
         <div className="room-content">
           <div className="room-image-box">
             <img src={image} alt={sala} className="room-image" />
-            <button className="room-favorite-button">❤️ Favoritar sala</button>
+            <button className="room-favorite-button"><FaRegHeart /> <b>Favoritar sala</b></button>
           </div>
           <div className="room-info">
-            <h2>{dados?.nome || "SALA"}</h2>
+            <h2>{dados.nome || "SALA"}</h2>
             <p>{dados?.descricao || "Sem descrição disponível."}</p>
             <p>
               <strong>Capacidade:</strong> {dados?.capacidade || "Indefinida"}
             </p>
-            <p>
-              <strong>Estado atual:</strong>
-              <TagStatus status={status} />
-            </p>
+            <div className="estado-atual">
+              <strong>Estado atual:</strong> <TagStatus status={status} />
+            </div>
           </div>
         </div>
 
