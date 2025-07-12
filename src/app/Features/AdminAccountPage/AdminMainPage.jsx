32d7c9de31d0dd78d662f6d6fb21acda39ec41c1
Ingryd <ingrydcduarte@gmail.com>
2025-07-11 21:38:53 -0300

    feat(wip): passing card functions in page (no funcionality)

--- src/app/Features/AdminAccountPage/AdminMainPage.jsx
+++ src/app/Features/AdminAccountPage/AdminMainPage.jsx
@@ -1,6 +1,10 @@
 import UserAccountHeader from "./components/UserAccountHeader";
 import { usuario } from "../../../models/UserModel";
-import './styles/AdminAccountStyle.css';
+import "./styles/AdminAccountStyle.css";
+import CardFuncionality from "./components/CardFunction";
+import { FaCalendar, FaUserPlus, FaUserGear } from "react-icons/fa6";
+import { BsFillDoorOpenFill } from "react-icons/bs";
+
 
 export default function AdminMainPage() {
   return (
@@ -10,6 +14,33 @@ export default function AdminMainPage() {
         <UserAccountHeader user={usuario[0]} />
       </section>
       <hr />
+
+      <section className="admin-functions">
+        <CardFuncionality
+          icon={<i className="fas fa-calendar-alt"><FaCalendar size={24} /></i>}
+          title="Controle de reservas"
+          description="Gerencie as reservas de salas."
+          onClick={() => console.log("Controle de reservas")}
+        />
+        <CardFuncionality
+          icon={<i className="fas fa-user-plus"><FaUserPlus size={24} /></i>}
+          title="Solicitações de registro"
+          description="Aprovar ou rejeitar solicitações de registro."
+          onClick={() => console.log("Solicitações de registro")}
+        />
+        <CardFuncionality
+          icon={<i className="fas fa-door-open"><BsFillDoorOpenFill size={24} /></i>}
+          title="Controle de salas"
+          description="Gerencie as salas disponíveis."
+          onClick={() => console.log("Controle de salas")}
+        />
+        <CardFuncionality
+          icon={<i className="fas fa-users"><FaUserGear size={24} /></i>}
+          title="Usuários na plataforma"
+          description="Visualize e gerencie usuários."
+          onClick={() => console.log("Usuários na plataforma")}
+        />
+      </section>
     </div>
   );
 }
