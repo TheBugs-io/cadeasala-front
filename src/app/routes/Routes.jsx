206cd9a45e900010111c4c6a0e84a9321f99df47
Ingryd <ingrydcduarte@gmail.com>
2025-07-12 10:28:27 -0300

    fix: just adjuments with comments in routes.jsx

--- src/app/routes/Routes.jsx
+++ src/app/routes/Routes.jsx
@@ -7,10 +7,12 @@ import ConfirmEmail from "../features/ConfirmEmail/ConfirmEmail";
 import ChooseType from "../features/RegisterPage/ChooseType";
 import RegisterFormDiscente from "../features/RegisterPage/RegisterFormDiscente";
 import RegisterDocenteForm from "../features/RegisterPage/RegisterDocenteForm";
-import DashboardRegistro from "../features/AdminDashboard/Dashboard";
-import { AuthProvider } from "../contexts/AuthContext";
-import PrivateRoute from "../Components/PrivateRoute";
 import MapaSalas from "../features/SMD-Maps/MapaSalas";
+{/* Rotas que precisam de autenticação, mas pra qualquer tipo */}
+import { AuthProvider } from "../contexts/AuthContext";
+{/* Rotas privadas / ADMIN */}
+import PrivateRoute from "../Components/PrivateRoute";
+import DashboardRegistro from "../features/AdminDashboard/Dashboard";
 import AdminMainPage from "../features/AdminAccountPage/AdminMainPage";
 
 export const router = createBrowserRouter([
