411f53a7e0038bf0cbb55728904d8d7a5eb4d7a8
ingrydf12 <ingrydcduarte@gmail.com>
2025-07-12 22:33:16 -0300

    fix: removing mocked infos in admin main page

--- src/app/Features/AdminAccountPage/AdminMainPage.jsx
+++ src/app/Features/AdminAccountPage/AdminMainPage.jsx
@@ -5,9 +5,10 @@ import { useNavigate } from "react-router-dom";
 import CardFuncionality from "./components/CardFunction";
 import { FaCalendar, FaUserPlus, FaUserGear } from "react-icons/fa6";
 import { BsFillDoorOpenFill } from "react-icons/bs";
-
+import { useAuth } from "../../contexts/AuthContext";
 
 export default function AdminMainPage() {
+  const { user } = useAuth();
   const navigate = useNavigate();
 
   const handleCardClick = (path) => {
@@ -18,31 +19,47 @@ export default function AdminMainPage() {
     <div className="admin-main-page">
       <section className="admin-header">
         <h1>Sua conta</h1>
-        <UserAccountHeader user={usuario[0]} />
+        <UserAccountHeader user={user} />
       </section>
       <hr />
 
       <section className="admin-functions">
         <CardFuncionality
-          icon={<i className="fas fa-calendar-alt"><FaCalendar size={24} /></i>}
+          icon={
+            <i className="fas fa-calendar-alt">
+              <FaCalendar size={24} />
+            </i>
+          }
           title="Controle de reservas"
           description="Gerencie as reservas e pedidos."
           onClick={() => console.log("Controle de reservas")}
         />
         <CardFuncionality
-          icon={<i className="fas fa-user-plus"><FaUserPlus size={24} /></i>}
+          icon={
+            <i className="fas fa-user-plus">
+              <FaUserPlus size={24} />
+            </i>
+          }
           title="Solicitações de registro"
           description="Aprovar ou rejeitar solicitações de registro."
           onClick={() => handleCardClick("/admin/dashboard")}
         />
         <CardFuncionality
-          icon={<i className="fas fa-door-open"><BsFillDoorOpenFill size={24} /></i>}
+          icon={
+            <i className="fas fa-door-open">
+              <BsFillDoorOpenFill size={24} />
+            </i>
+          }
           title="Controle de salas"
           description="Gerencie as salas do bloco IUVI."
           onClick={() => console.log("Controle de salas")}
         />
         <CardFuncionality
-          icon={<i className="fas fa-users"><FaUserGear size={24} /></i>}
+          icon={
+            <i className="fas fa-users">
+              <FaUserGear size={24} />
+            </i>
+          }
           title="Usuários na plataforma"
           description="Visualize e gerencie usuários dentro da plataforma."
           onClick={() => console.log("Usuários na plataforma")}
