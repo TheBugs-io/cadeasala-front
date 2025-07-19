1cd52628ec37842c9eef8f7d68f58c071ffa84c2
Ingryd <ingrydcduarte@gmail.com>
2025-07-19 09:48:44 -0300

    test: mocked pedidos reserva info (json)

---
+++ src/models/ExemploPedido.js
@@ -0,0 +1,27 @@
+export const examples = [
+  {
+    title: "Laboratório 05",
+    subtitle: "Terça (14h-16h)",
+    dateRange: "17/07/25 - 18/07/25",
+    topLeftLabel: "Renanzinho",
+    topRightLabel: "Discente",
+    bottomLabel: "Outro",
+    bottomLabelColor: "#8B5CF6",
+  },
+  {
+    title: "Laboratório 03",
+    subtitle: "Segunda (10h-12h)",
+    dateRange: "15/07/25 - 16/07/25",
+    topLeftLabel: "Maria Silva",
+    topRightLabel: "Docente",
+    bottomLabel: "Física",
+    bottomLabelColor: "#059669",
+  },
+  {
+    title: "Sala 201",
+    subtitle: "Quarta (16h-18h)",
+    dateRange: "19/07/25 - 20/07/25",
+    topLeftLabel: "João Santos",
+    topRightLabel: "Monitor",
+  },
+];
