5bceea2c54904d4fd50858c80e88c99d0c8fa7c5
Ingryd <ingrydcduarte@gmail.com>
2025-07-20 15:49:10 -0300

    feat(wip): service api int> (reservas)

---
+++ src/app/service/admin/reservasService.js
@@ -0,0 +1,33 @@
+import api from '../api';
+
+export const fetchReservas = async () => {
+  try {
+    const response = await api.get(`/reservas`);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar reservas:", error);
+    throw error;
+  }
+};
+
+//TODO: Criar uma reserva definindo um responsável
+export const createReserva = async (reservaData) => {
+  try {
+    const response = await api.post(`/reservas`, reservaData);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao criar reserva:", error);
+    throw error;
+  }
+};
+
+//TODO: Deletar reserva
+export const deleteReserva = async (reservaId) => {
+  try {
+    const response = await api.delete(`/reservas/${reservaId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao deletar reserva:", error);
+    throw error;
+  }
+};
