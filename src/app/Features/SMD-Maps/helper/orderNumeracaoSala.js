6cb55dd493591bde9db04dd958d8533cd15e08ad
Ingryd <ingrydcduarte@gmail.com>
2025-07-24 01:14:25 -0300

    refactor: order numeracaoSala for mapa

--- src/app/Features/SMD-Maps/helper/orderNumeracaoSala.js
+++ src/app/Features/SMD-Maps/helper/orderNumeracaoSala.js
@@ -1,15 +1,21 @@
-export const ordenarPorNumeracaoSala = (a, b) => {
-  const regex = /^(\d+)([A-Z])$/;
+export function ordenarPorNumeracaoSala(a, b) {
+  const extrairNumeroLetra = (numeracao) => {
+    const regex = /^(\d+)([A-Z])$/i;
+    const match = numeracao.match(regex);
+    if (!match) {
+      return [Number.MAX_SAFE_INTEGER, 'Z'];
+    }
+    return [parseInt(match[1], 10), match[2].toUpperCase()];
+  };
 
-  const [, numA, letraA] = a.numeracaoSala.match(regex) || [];
-  const [, numB, letraB] = b.numeracaoSala.match(regex) || [];
+  const [numA, letraA] = extrairNumeroLetra(a.numeracaoSala);
+  const [numB, letraB] = extrairNumeroLetra(b.numeracaoSala);
 
-  const numeroA = parseInt(numA);
-  const numeroB = parseInt(numB);
+  const ordemLinha = { 'E': 0, 'D': 1 };
 
-  if (numeroA !== numeroB) {
-    return numeroA - numeroB;
+  if (numA !== numB) {
+    return numA - numB;
   }
 
-  return letraA.localeCompare(letraB);
-};
+  return (ordemLinha[letraA] ?? 99) - (ordemLinha[letraB] ?? 99);
+}
