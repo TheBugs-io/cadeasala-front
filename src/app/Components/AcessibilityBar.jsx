2170d2306d1c0da5ae616d925f273371d43d3695
Ingryd <ingrydcduarte@gmail.com>
2025-07-26 00:06:16 -0300

    refactor: acessibility bar

--- src/app/Components/AcessibilityBar.jsx
+++ src/app/Components/AcessibilityBar.jsx
@@ -1,47 +1,42 @@
-import { useState, useEffect } from "react";
 import "../styles/AcessibilityBar.css";
-import { IoAccessibilitySharp, IoContrastOutline } from "react-icons/io5";
+import { IoContrastOutline } from "react-icons/io5";
 import brazilIcon from "../assets/icons/brazilVector.svg";
+import { useAcessibilidade } from "../contexts/AcessibilityContext";
 
 const AccessibilityBar = () => {
-  const [fontSize, setFontSize] = useState(100);
-  const [highContrast, setHighContrast] = useState(false);
-
-  useEffect(() => {
-    document.body.style.fontSize = `${fontSize}%`;
-    document.body.classList.toggle("high-contrast", highContrast);
-  }, [fontSize, highContrast]);
-
-  const increaseFont = () => setFontSize((prev) => Math.min(prev + 10, 150));
-  const decreaseFont = () => setFontSize((prev) => Math.max(prev - 10, 70));
-  const toggleContrast = () => setHighContrast((prev) => !prev);
-  const resetAccessibility = () => {
-    setFontSize(100);
-    setHighContrast(false);
-  };
+  const {
+    increaseFont,
+    decreaseFont,
+    toggleContrast,
+    highContrast,
+  } = useAcessibilidade();
 
   return (
     <div className="accessibility-bar">
       <div className="acessibility-container">
-      <div className="acessibility-actions">
-      <a accessKey="1" href="#mainContent" title="Ir para o conteúdo">
-        <b style={{ fontWeight: "bold", textDecoration: "underline" }} lang="pt-br">Ir para o conteúdo</b> <b style={{ fontWeight: "bold" }}>[1]</b>
-      </a>
-      {/* Mudar depois para o ID do conteudo / botão */}
-      <a accessKey="2" href="#footer" title="Ir para o mapa">
-        <b style={{ fontWeight: "bold", textDecoration: "underline" }} lang="pt-br">Ir para o mapa</b> <b style={{ fontWeight: "bold" }}>[2]</b>
-      </a>
-      </div>
-      <div className="acessibility-options">
-        <button onClick={increaseFont}>A+</button>
-        <button onClick={decreaseFont}>A-</button>
-        <button onClick={toggleContrast}>
-          <IoContrastOutline />
-          {highContrast ? "Modo Claro" : "Alto Contraste"}
-        </button>
-        <hr />
-        <img src={brazilIcon} alt="Bandeira do Brasil representa a linguagem do site." />
-      </div>
+        <div className="acessibility-actions">
+          <a accessKey="1" href="#mainContent" title="Ir para o conteúdo">
+            <b style={{ textDecoration: "underline" }}>Ir para o conteúdo</b>{" "}
+            <b>[1]</b>
+          </a>
+          <a accessKey="2" href="#footer" title="Ir para o rodapé">
+            <b style={{ textDecoration: "underline" }}>Ir para o rodapé</b>{" "}
+            <b>[2]</b>
+          </a>
+        </div>
+        <div className="acessibility-options">
+          <button onClick={increaseFont}>A+</button>
+          <button onClick={decreaseFont}>A-</button>
+          <button onClick={toggleContrast}>
+            <IoContrastOutline />
+            {highContrast ? "Modo Claro" : "Alto Contraste"}
+          </button>
+          <hr />
+          <img
+            src={brazilIcon}
+            alt="Bandeira do Brasil representa a linguagem do site."
+          />
+        </div>
       </div>
     </div>
   );
